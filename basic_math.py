d302b6e738dd2ddd2337bfc2d47afe8de8ed0ad3
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-02-14 17:52:34 +0000

    added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: Feb 14, 2025
+# Basic Math Operators
+
+
+def main():
+    print("77+33 = {}".format(77 + 33))  # Addition
+    print("100-1 = {}".format(100 - 1))  # Subtraction
+    print("103*204 = {}".format(103 * 204))  # Multiplication
+    print("10/2 = {:.0f}".format(10 / 2))  # Division
+    print("3\u00b2 = {}".format(3**2))  # Exponentiation
+    print("\u221a16 = {:.0f}".format(16**0.5))  # Square Root
+    print("19//9 = {}".format(19 // 9))  # Floor Division
+    print("mod\u2083(707) = {}".format(707 % 3))  # Modulo
+
+
+if __name__ == "__main__":
+    main()
